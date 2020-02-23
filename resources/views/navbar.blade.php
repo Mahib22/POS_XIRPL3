03eb02ec0a60db0fc048f1a9c3e6db1981f944a7
Mahib22 <mahib.arib@gmail.com>
2020-02-23 20:59:39 +0700

    first

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                LOGO
+            </a>
+        </div>
+        
+        <ul class="nav navbar-nav">
+            <li class="active">
+                <a class="nav-link" href="/blog">Home</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/blog/about">About</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/blog/contact">Contact</a>
+            </li>
+        <ul>
+        
+    </div>
+</nav>
